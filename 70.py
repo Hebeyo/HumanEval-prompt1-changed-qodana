b546ee6641be293f570287c3a59c6027767413b1
Shuang Li <51077997+Hebeyo@users.noreply.github.com>
2024-05-07 15:16:44 +0800

    Initial commit

---
+++ 70.py
@@ -0,0 +1,13 @@
+def strange_sort_list(lst):
+    res = []
+    switch = True
+    while lst:
+        if switch:
+            res.append(min(lst))
+            lst.remove(min(lst))
+            switch = False
+        else:
+            res.append(max(lst))
+            lst.remove(max(lst))
+            switch = True
+    return res
